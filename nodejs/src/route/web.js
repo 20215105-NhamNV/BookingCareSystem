538c283ad799296e97dbec9687c8181fdd0fb6b7
Nguyen Van Nham <nhampmq@gmail.com>
2024-03-15 12:43:16 +0700

    start write REST API

--- nodejs/src/route/web.js
+++ nodejs/src/route/web.js
@@ -15,7 +15,7 @@ let initWebRouters = (app) => {
     router.post('/put-crud', homeController.putCRUD);
     router.get('/delete-crud', homeController.deleteCRUD);
 
-    router.post('/api/login', userController.handleLogin);
+    router.post('/api/login', userController.handleLogin);//
     return app.use("/", router);
 }
 
