d0c75ad75143b87202304d8d13d0e1938d8f0044
Nguyen Van Nham <nhampmq@gmail.com>
2024-03-26 21:37:26 +0700

    read

--- Backend-NodeJS/src/controllers/userController.js
+++ Backend-NodeJS/src/controllers/userController.js
@@ -26,8 +26,27 @@ let handleLogin = async (req, res) => {
     })
 }
 
+let handleGetAllUsers = async (req, res) => {
+    let id = req.query.id;//ALL, ID
 
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'missing required parameters',
+            users: []
+        })
+    }
+
+    let users = await userService.getAllUsers(id);
+    console.log(users)
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'ok',
+        users
+    })
+}
 
 module.exports = {
     handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers,
 }
